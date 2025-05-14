e87227cd9466e1850f11c45d3ea92303de231383
HenrySilverio <silveriohenriqueb@gmail.com>
2025-05-14 09:25:34 -0300

    Updates favicon and Tailwind configuration
    Replaces favicon with SVG format and updates references in HTML.
    Adds dark mode support and custom theme colors to Tailwind configuration.
    Expands content paths for better utility in Tailwind.
    
    Improves maintainability and user experience.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-blue': '#1f104f',
+        'brand-off-white': '#fffafa',
+        'brand-cart-bg': '#d3d3d3',
+        // Dark theme colors
+        'dark-brand-blue': '#6D5BA7',
+        'dark-brand-off-white': '#18181b',
+        'dark-brand-cart-bg': '#27272a',
+      },
+    },
   },
   plugins: [],
 }
